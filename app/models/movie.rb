700993bd6b682e3229eba79d781c37b05866f433
ConiMarchant <cbmarchant@miuandes.cl>
2018-05-15 13:57:36 -0400

    Association (I think) ready

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
   belongs_to :director
   belongs_to :address
+  has_and_belongs_to_many :categories
+  has_many :actormovies
+  has_many :actors, through: :actormovies
 end
