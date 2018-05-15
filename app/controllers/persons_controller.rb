fd83b837cdb1baeceeec9db433fe2fc667bab1c4
ConiMarchant <cbmarchant@miuandes.cl>
2018-05-15 15:14:04 -0400

    commiting so alf can work

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,4 +1,11 @@
 class ActorsController < ApplicationController
 
+  def new
 
+  end
+
+  private
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :birthday, :description)
+  end
 end
